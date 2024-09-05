78b37226e59fb1cb57c02f41ddf013263c2276f5
ALUMNOP-104 <ALUMNOP-104@UCSP.AULAB>
2024-09-05 08:00:04 -0500

    ejercicio 3.10

--- semana_3/ejercicios_cap3/ejercicio3-10/InvoiceTest.cpp
+++ semana_3/ejercicios_cap3/ejercicio3-10/InvoiceTest.cpp
@@ -14,16 +14,16 @@ int main() {
     Invoice invoice2{"MT540", "Llave de Mano",
             3, 20, -0.25, -0.12};
 
-    cout << "Recibo 1: Num. de Parte\t\tDescripcion\tCantidad"<< 
+    cout << "Recibo 1 \nNum. de Parte\t\tDescripcion\tCantidad"<< 
             "\tPrecio x Item\tIVA\tDescuento\tTotal\n";
-    cout << "\t  " << invoice1.getNumeroParte() << "\t\t\t" << invoice1.getDescripcion() << "\t"
+    cout << invoice1.getNumeroParte() << "\t\t\t" << invoice1.getDescripcion() << "\t"
         << invoice1.getItemCant() << "\t\t"  << invoice1.getPrecioPorItem() << "\t\t" << invoice1.getVAT() * 100
         << "%\t" << invoice1.getTazaDescuento()*100 << "%\t\t" << invoice1.getInvoiceAmount()<<"\n";
 
 
-    cout << "\nRecibo 2: Num. de Parte\t\tDescripcion\tCantidad"<< 
+    cout << "\nRecibo 2 \nNum. de Parte\t\tDescripcion\tCantidad"<< 
             "\tPrecio x Item\tIVA\tDescuento\tTotal\n";
-    cout << "\t  " << invoice2.getNumeroParte() << "\t\t\t" << invoice2.getDescripcion() << "\t"
+    cout << invoice2.getNumeroParte() << "\t\t\t" << invoice2.getDescripcion() << "\t"
         << invoice2.getItemCant() << "\t\t"  << invoice2.getPrecioPorItem() << "\t\t" << invoice2.getVAT() * 100
         << "%\t" << invoice2.getTazaDescuento()*100 << "%\t\t" << invoice2.getInvoiceAmount()<<"\n";
     return 0;
